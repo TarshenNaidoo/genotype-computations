de50fba55ff005cab0cff2f7ca27b1cf5da63a11
TarshenNaidoo <58662650+TarshenNaidoo@users.noreply.github.com>
2023-04-03 01:52:35 +0200

    Updated algorithm

--- assignment 1/wink.rs
+++ assignment 1/wink.rs
@@ -1,22 +1,31 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::env;
+use std::io::Read;
 
 const BUF_SIZE: usize = 1000;
 
-fn missing_arthritis<P: AsRef<Path>>(filename: P) -> std::io::Result<Vec<Vec<u8>>> {
+fn missing(filename: &str) {
 
     let file_base_name = filename; // replace with the actual file name without the extension
     let bed_file_name = format!("{}.bed", file_base_name);
     let fam_file_name = format!("{}.fam", file_base_name);
     let bim_file_name = format!("{}.bim", file_base_name);
 
-    let bed_file = File::open(&bed_file_name).expect("Failed to open bed file");
+    let mut bed_file = File::open(&bed_file_name).expect("Failed to open bed file");
     let bim_file = File::open(&fam_file_name).expect("Failed to open bim file");
     let fam_file = File::open(&bim_file_name).expect("Failed to open fam file");
 
     // read the number of individuals and SNPs
-    let mut num_individuals = BufReader::new(fam_file).lines().count();
-    let mut num_snps = BufReader::new(bim_file).lines().count();
+    let num_individuals = BufReader::new(fam_file).lines().count();
+    let num_snps = BufReader::new(bim_file).lines().count();
+
+    
+    // read the magic number and file format version
+    let mut reader = BufReader::new(bed_file);
+    let mut magic_number = [0u8; 2];
+    reader.read_exact(&mut magic_number);
+    let third_byte = reader.read_u8();
 
     // Open the bed file and read in the genotypes
     let mut buf = [0u8; BUF_SIZE];
@@ -34,6 +43,8 @@ fn missing_arthritis<P: AsRef<Path>>(filename: P) -> std::io::Result<Vec<Vec<u8>
             }
         }
     }
+
+    println!(third_byte)
 
     // Compute the per-individual missingness
     let mut missingness = Vec::new();
@@ -55,6 +66,19 @@ fn missing_arthritis<P: AsRef<Path>>(filename: P) -> std::io::Result<Vec<Vec<u8>
     }
 }
 
+
+fn aaf(filename: &str) {
+    println!("Called aaf with argument: {}", filename);
+}
+
+fn trans10(filename: &str) {
+    println!("Called trans10 with argument: {}", filename);
+}
+
+fn trans01(filename: &str) {
+    println!("Called trans01 with argument: {}", filename);
+}
+
 fn main() {
     
     // Get the command line arguments
@@ -66,29 +90,14 @@ fn main() {
         std::process::exit(1);
     }
 
-    // Open the input files
-    // let bed_file = File::open(&args[1]).expect("Failed to open bed file");
-    // let bim_file = File::open(&args[2]).expect("Failed to open bim file");
-    // let fam_file = File::open(&args[3]).expect("Failed to open fam file");
+    let option = &args[1];
+    let string = &args[2];
 
-    let command = & args[1];
-
-    match command {
-        "missing" => {
-            missing_arthritis(filename)?;
-        },
-        "aaf" => {
-            // Code to handle the 'aaf' case
-        },
-        "trans10" => {
-            // Code to handle the 'trans10' case
-        },
-        "trans01" => {
-            // Code to handle the 'trans01' case
-        },
-        _ => {
-            eprintln!("Invalid command Usage: {} <command: missing, aaf, trans10, trans01>", args[0]);
-            std::process::exit(1);
-        }
+    match option.as_str() {
+        "missing" => missing(string),
+        "aaf" => aaf(string),
+        "trans10" => trans10(string),
+        "trans01" => trans01(string),
+        _ => println!("Invalid option: {}", option),
     }
 }
