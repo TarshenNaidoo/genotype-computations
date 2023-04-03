3ab941b8d6e8eaa820d8099cf834523c03b082e2
TarshenNaidoo <58662650+TarshenNaidoo@users.noreply.github.com>
2023-04-03 17:51:34 +0200

    Added trans10 function

--- assignment 1/wink.rs
+++ assignment 1/wink.rs
@@ -54,7 +54,7 @@ fn missing(prefix: &str){
                 // }
                 // println!("{:?}", buf);
                 for k in 0..4 {
-                    if j * 4 + k > num_individuals {
+                    if j * 4 + k > num_individuals - 1{
                         break;
                     }
                     // let bit_idx = j % 4;
@@ -147,7 +147,7 @@ fn aaf(prefix: &str) {
                 // }
                 // println!("{:?}", buf);
                 for k in 0..4 {
-                    if j * 4 + k > num_individuals {
+                    if j * 4 + k > num_individuals - 1 {
                         break;
                     }
                     // let bit_idx = j % 4;
@@ -194,7 +194,7 @@ fn aaf(prefix: &str) {
     }
 }
 
-fn trans10(filename: &str) {
+fn trans10(prefix: &str) {
     // Open bed, bim, and fam files
     let bed_file = File::open(format!("{}.bed", prefix)).unwrap();
     let mut bed_reader = BufReader::new(bed_file);
@@ -206,16 +206,16 @@ fn trans10(filename: &str) {
     let fam_reader = BufReader::new(fam_file);
 
     // Ignore first two bytes of bed file
-    let mut buf = [0; 2];
-    bed_reader.read_exact(&mut buf).unwrap();
+    let mut magic_buf = [0; 2];
+    bed_reader.read_exact(&mut magic_buf).unwrap();
 
     // Read third byte to determine snp major
     let mut snp_major_buf = [0; 1];
     bed_reader.read_exact(&mut snp_major_buf).unwrap();
     let snp_major = snp_major_buf[0] == 1;
 
-    if snp_major {
-        panic!("bed file is already in SNP-major format!");
+    if !snp_major {
+        panic!("bed file is already in individual-major format!");
     }
 
     // Calculate number of individuals and number of SNPs
@@ -224,8 +224,7 @@ fn trans10(filename: &str) {
     println!("num_individuals: {}", num_individuals);
     println!("num_snps: {}", num_snps);
 
-    let mut missing_nums = Vec::with_capacity(num_individuals);
-    missing_nums.extend(vec![0.0; num_individuals]);
+    let mut bed_vec = vec![vec![0 ; num_individuals] ; num_snps];
 
     let mut j_iter = (num_individuals/4) as usize;
 
@@ -234,7 +233,7 @@ fn trans10(filename: &str) {
     }
 
     if snp_major {
-        for _i in 0..num_snps {
+        for i in 0..num_snps {
             for j in 0..(j_iter) {
                 // let byte_idx = if is_snp_major == 1 {
                 //     j / 4
@@ -249,19 +248,17 @@ fn trans10(filename: &str) {
                 // }
                 // println!("{:?}", buf);
                 for k in 0..4 {
-                    if j * 4 + k > num_individuals {
+                    if j * 4 + k > num_individuals - 1 {
                         break;
                     }
                     // let bit_idx = j % 4;
-
                     // if j == 0 {
                     //     println!("shift: {}", format!("{:#010b}", buf[0] >> k*2));
                     // }
                     let genotype = (buf[0] >> k*2) & 0b11;
-                    // println!("{:?}", genotype & 0b11);
-                    if genotype == 0b01 {
-                        missing_nums[j * 4 + k] += 1.0;
-                    }
+                    // println!(" {:?} ", j * 4 + k);
+                    bed_vec[i][j * 4 + k] = genotype;
+                    // print!(" {:?} ", bed_vec[i][j * 4 + k]);
 
                     // print!("[");
                     // for element in missing_nums.iter_mut() {
@@ -276,17 +273,73 @@ fn trans10(filename: &str) {
 
     }
 
-    for element in missing_nums.iter_mut() {
-        *element /= num_snps as f64;
+    let mut tbed_vec = vec![vec![0; num_snps]; num_individuals];
+    // println!("{:?}", bed_vec);
+
+    for i in 0..num_snps {
+        for j in 0..num_individuals {
+            tbed_vec[j][i] = bed_vec[i][j];
+        }
     }
 
-    let mut file = File::create(format!("{}.imiss", prefix)).unwrap();
-    for element in &missing_nums {
-        let element_str = element.to_string();
-        let element_bytes = element_str.as_bytes();
-        file.write_all(element_bytes).unwrap();
-        file.write_all(b"\n").unwrap();
+    // let mut ind_major = Vec::new();
+
+    let mut j_iter = (num_snps/4) as usize;
+
+    if num_snps % 4 > 0 {
+        j_iter += 1;
     }
+    // println!("j_iter {:?}", j_iter );
+
+    let mut file = File::create(format!("{}.tbed", prefix)).unwrap();
+
+
+    file.write(&magic_buf).unwrap();
+    file.write(&[0u8; 1]).unwrap();
+
+    for i in 0..num_individuals {
+        for j in 0..(j_iter) {
+            let mut buf = Vec::new();
+            let mut byte: u8 = 0;
+            for k in 0..4 {
+                if j * 4 + k > num_snps - 1 {
+                    break;
+                }
+                // println!("{:?}", j * 4 + k );
+                byte += tbed_vec[i][j * 4 + k] << 2 * (3 - k);
+            }
+
+            print!(" {} ", j);
+            println!("shift: {} ", format!("{:#010b}", byte));
+            buf.push(byte);
+
+            file.write(&buf).unwrap();
+
+        }
+    }
+    
+    // for i in 0..num_snps {
+    //     for j in 0..num_individuals {
+    //         tbed_vec[j][i] = bed_vec[i][j];
+    //     }
+    // }
+    
+    // let mut file = File::create(format!("{}.tbed", prefix)).unwrap();
+    // println!("{:?}", bed_vec[0][0]);
+    // for i in 0..num_individuals {
+    //     for j in 0..num_snps {
+    //         file.write(&[tbed_vec[i][j]]).unwrap();
+    //         // transposed_vec2d[j][i] = vec2d[i][j];
+    //     }
+    // }
+
+    // let mut file = File::create(format!("{}.tbed", prefix)).unwrap();
+    // for element in &tbed_vec {
+    //     let element_str = element.to_string();
+    //     let element_bytes = element_str.as_bytes();
+    //     file.write_all(element_bytes).unwrap();
+    //     file.write_all(b"\n").unwrap();
+    // }
 }
 
 fn trans01(filename: &str) {
