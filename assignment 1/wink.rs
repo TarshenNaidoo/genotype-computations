50d805c0cd1c559eddfe3ee93c1382eef203f976
TarshenNaidoo <58662650+TarshenNaidoo@users.noreply.github.com>
2023-04-03 15:28:37 +0200

    Updated algorithm

--- assignment 1/wink.rs
+++ assignment 1/wink.rs
@@ -1,71 +1,98 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::io::{BufRead, BufReader, Read};
 use std::env;
-use std::io::Read;
+use std::io::prelude::*;
 
-const BUF_SIZE: usize = 1000;
+fn missing(prefix: &str){
+    // Open bed, bim, and fam files
+    let bed_file = File::open(format!("{}.bed", prefix)).unwrap();
+    let mut bed_reader = BufReader::new(bed_file);
 
-fn missing(filename: &str) {
+    let bim_file = File::open(format!("{}.bim", prefix)).unwrap();
+    let bim_reader = BufReader::new(bim_file);
 
-    let file_base_name = filename; // replace with the actual file name without the extension
-    let bed_file_name = format!("{}.bed", file_base_name);
-    let fam_file_name = format!("{}.fam", file_base_name);
-    let bim_file_name = format!("{}.bim", file_base_name);
+    let fam_file = File::open(format!("{}.fam", prefix)).unwrap();
+    let fam_reader = BufReader::new(fam_file);
 
-    let mut bed_file = File::open(&bed_file_name).expect("Failed to open bed file");
-    let bim_file = File::open(&fam_file_name).expect("Failed to open bim file");
-    let fam_file = File::open(&bim_file_name).expect("Failed to open fam file");
+    // Ignore first two bytes of bed file
+    let mut buf = [0; 2];
+    bed_reader.read_exact(&mut buf).unwrap();
 
-    // read the number of individuals and SNPs
-    let num_individuals = BufReader::new(fam_file).lines().count();
-    let num_snps = BufReader::new(bim_file).lines().count();
+    // Read third byte to determine snp major
+    let mut snp_major_buf = [0; 1];
+    bed_reader.read_exact(&mut snp_major_buf).unwrap();
+    let snp_major = snp_major_buf[0] == 1;
 
+    // Calculate number of individuals and number of SNPs
+    let num_individuals = fam_reader.lines().count();
+    let num_snps = bim_reader.lines().count();
+    println!("num_individuals: {}", num_individuals);
+    println!("num_snps: {}", num_snps);
+
+    let mut missing_nums = Vec::with_capacity(num_individuals);
+    missing_nums.extend(vec![0.0; num_individuals]);
+
+    let mut j_iter = (num_individuals/4) as usize;
+
+    if num_individuals % 4 > 0 {
+        j_iter += 1;
+    }
+
+    if snp_major {
+        for _i in 0..num_snps {
+            for j in 0..(j_iter) {
+                // let byte_idx = if is_snp_major == 1 {
+                //     j / 4
+                // } else {
+                //     i / 4
+                // } as usize;
+                let mut buf = [0u8; 1];
+                // println!("{}", (_i * j));
+                bed_reader.read_exact(&mut buf).unwrap();
+                // if j == 0 {
+                //     println!("binary: {}", format!("{:b}", buf[0]));
+                // }
+                // println!("{:?}", buf);
+                for k in 0..4 {
+                    if j * 4 + k > num_individuals {
+                        break;
+                    }
+                    // let bit_idx = j % 4;
+
+                    // if j == 0 {
+                    //     println!("shift: {}", format!("{:#010b}", buf[0] >> k*2));
+                    // }
+                    let genotype = (buf[0] >> k*2) & 0b11;
+                    // println!("{:?}", genotype & 0b11);
+                    if genotype == 0b01 {
+                        missing_nums[j * 4 + k] += 1.0;
+                    }
+
+                    // print!("[");
+                    // for element in missing_nums.iter_mut() {
+                    //      print!(" {} ", element);
+                    // }
+                    // println!("]");
     
-    // read the magic number and file format version
-    let mut reader = BufReader::new(bed_file);
-    let mut magic_number = [0u8; 2];
-    reader.read_exact(&mut magic_number);
-    let third_byte = reader.read_u8();
-
-    // Open the bed file and read in the genotypes
-    let mut buf = [0u8; BUF_SIZE];
-    let mut genotypes = Vec::new();
-    loop {
-        let bytes_read = bed_file.read(&mut buf).unwrap();
-        if bytes_read == 0 {
-            break;
-        }
-        for i in 0..bytes_read {
-            let byte = buf[i];
-            for bit_idx in (0..8).step_by(2) {
-                let genotype = (byte >> bit_idx) & 0b11;
-                genotypes.push(genotype);
+                }
             }
         }
+    } else {
+
     }
 
-    println!(third_byte)
-
-    // Compute the per-individual missingness
-    let mut missingness = Vec::new();
-    for i in 0..num_individuals {
-        let start_idx = i * num_snps;
-        let end_idx = start_idx + num_snps;
-        let num_missing = genotypes[start_idx..end_idx]
-            .iter()
-            .filter(|&&genotype| genotype == 0b11)
-            .count();
-        let missingness_rate = num_missing as f64 / num_snps as f64;
-        missingness.push(missingness_rate);
+    for element in missing_nums.iter_mut() {
+        *element /= num_snps as f64;
     }
 
-    // Write the missingness results to a file
-    let mut imiss_file = BufWriter::new(File::create("arthritis.imiss").unwrap());
-    for m in missingness {
-        let _ = write!(imiss_file, "{:.4}\n", m);
+    let mut file = File::create(format!("{}.imiss", prefix)).unwrap();
+    for element in &missing_nums {
+        let element_str = element.to_string();
+        let element_bytes = element_str.as_bytes();
+        file.write_all(element_bytes).unwrap();
+        file.write_all(b"\n").unwrap();
     }
 }
-
 
 fn aaf(filename: &str) {
     println!("Called aaf with argument: {}", filename);
